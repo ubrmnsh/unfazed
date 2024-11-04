465448a59c1e87b5cca36d6e45bdc01d6c2eb0bd
ubrmnsh <srxydv@gmail.com>
2024-11-05 00:15:15 +0530

    Initial Commit

---
+++ unfazedTests/unfazedTests.swift
@@ -0,0 +1,17 @@
+//
+//  unfazedTests.swift
+//  unfazedTests
+//
+//  Created by Saurabh  on 05/11/24.
+//
+
+import Testing
+@testable import unfazed
+
+struct unfazedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
